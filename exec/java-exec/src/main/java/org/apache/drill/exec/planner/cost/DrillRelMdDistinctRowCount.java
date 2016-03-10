a8a96b8d976203ca68dae3b7178ebeb6fb165bbf
Jinfeng Ni <jni@apache.org>
2016-03-10 11:09:58 -0800

    DRILL-4347: Improve Relmetadata call for query planning.

--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/cost/DrillRelMdDistinctRowCount.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/cost/DrillRelMdDistinctRowCount.java
@@ -17,6 +17,7 @@
  ******************************************************************************/
 package org.apache.drill.exec.planner.cost;
 
+import com.google.common.base.Preconditions;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.metadata.ReflectiveRelMetadataProvider;
 import org.apache.calcite.rel.metadata.RelMdDistinctRowCount;
@@ -25,6 +26,7 @@ import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.util.BuiltInMethod;
 import org.apache.calcite.util.ImmutableBitSet;
 import org.apache.drill.exec.planner.logical.DrillScanRel;
+import org.apache.drill.exec.planner.physical.ScanPrel;
 
 public class DrillRelMdDistinctRowCount extends RelMdDistinctRowCount{
   private static final DrillRelMdDistinctRowCount INSTANCE =
@@ -36,14 +38,16 @@ public class DrillRelMdDistinctRowCount extends RelMdDistinctRowCount{
 
   @Override
   public Double getDistinctRowCount(RelNode rel, ImmutableBitSet groupKey, RexNode predicate) {
-    if (rel instanceof DrillScanRel) {
-      return getDistinctRowCount((DrillScanRel) rel, groupKey, predicate);
+    if (rel instanceof DrillScanRel || rel instanceof ScanPrel) {
+      return getScanDistinctRowCount(rel, groupKey, predicate);
     } else {
       return super.getDistinctRowCount(rel, groupKey, predicate);
     }
   }
 
-  private Double getDistinctRowCount(DrillScanRel scan, ImmutableBitSet groupKey, RexNode predicate) {
+  private Double getScanDistinctRowCount(RelNode scan, ImmutableBitSet groupKey, RexNode predicate) {
+    Preconditions.checkArgument(scan instanceof DrillScanRel || scan instanceof  ScanPrel);
+
     // Consistent with the estimation of Aggregate row count in RelMdRowCount : distinctRowCount = rowCount * 10%.
     return scan.getRows() * 0.1;
   }
