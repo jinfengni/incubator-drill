1b32f5d6ab34ffe6b2e19d08e548191c17aca225
Serhii-Harnyk <serhii.harnyk@gmail.com>
2017-03-03 15:24:26 +0000

    MD-1447: Drill Query Planner stuck at STARTING phase if the query having complexed where conditions with many "or" on the same column

--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/physical/explain/NumberingRelWriter.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/physical/explain/NumberingRelWriter.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -68,9 +68,8 @@ class NumberingRelWriter implements RelWriter {
       inputs = FlatLists.of(joinPrel.getRight(), joinPrel.getLeft());
     }
 
-    if (!RelMetadataQuery.isVisibleInExplain(
-        rel,
-        detailLevel)) {
+    RelMetadataQuery mq = RelMetadataQuery.instance();
+    if (!mq.isVisibleInExplain(rel, detailLevel)) {
       // render children in place of this, at same level
       explainInputs(inputs);
       return;
@@ -117,9 +116,9 @@ class NumberingRelWriter implements RelWriter {
     if (detailLevel == SqlExplainLevel.ALL_ATTRIBUTES) {
       s.append(" : rowType = " + rel.getRowType().toString());
       s.append(": rowcount = ")
-          .append(RelMetadataQuery.getRowCount(rel))
+          .append(mq.getRowCount(rel))
           .append(", cumulative cost = ")
-          .append(RelMetadataQuery.getCumulativeCost(rel));
+          .append(mq.getCumulativeCost(rel));
        s.append(", id = ").append(rel.getId());
     }
     pw.println(s);
